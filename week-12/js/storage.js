67bbad835f480490f69285a476fd3c21566bbb83
Thomas Xuan <txuan@iu.edu>
2025-04-02 16:52:45 -0400

    Continuing work on notes

--- week-12/js/storage.js
+++ week-12/js/storage.js
@@ -3,6 +3,11 @@ const loginFormRef = document.querySelector("#login");
 const usernameRef = document.querySelector("#username");
 const passwordRef = document.querySelector("#password");
 
+const contentRef = document.querySelector("#content");
+const logoutRef = document.querySelector("#logout");
+
+let activeUser = JSON.parse(localStorage.getItem("activeUser") || "{}");
+
 function loginUser(e) {
   e.preventDefault();
   const usernameValue = usernameRef.value;
@@ -29,6 +34,10 @@ function loginUser(e) {
     //make user
     users.push({ username: usernameValue, password: passwordValue });
     localStorage.setItem("users", JSON.stringify(users));
+    alert("New User Created");
+
+    activeUser = { username: usernameValue };
+    localStorage.setItem("activeUser", JSON.stringify(activeUser));
   }
   //wrong password
   else if (correctPassword === false) {
@@ -37,8 +46,28 @@ function loginUser(e) {
   //You are logged in
   else {
     alert("Successfully logged in");
+    activeUser = { username: usernameValue };
+    localStorage.setItem("activeUser", JSON.stringify(activeUser));
   }
-  console.log(users);
+  toggleLogin();
 }
 
+function toggleLogin() {
+  if (!activeUser.username) {
+    loginFormRef.style.display = "block";
+    contentRef.style.display = "none";
+  } else {
+    loginFormRef.style.display = "none";
+    contentRef.style.display = "block";
+  }
+}
+
+function logoutUser() {
+  localStorage.removeItem("activeUser");
+  activeUser = {};
+
+  toggleLogin();
+}
 loginFormRef.onsubmit = loginUser;
+logoutRef.onclick = logoutUser;
+toggleLogin();
